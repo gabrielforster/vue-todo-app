27d4b9fc8e3b000e8799bb55d94b03275db228b5
gabrielforster <rochafrgabriel@gmail.com>
2022-04-23 03:26:39 -0300

    first commit

--- README.md
+++ README.md
@@ -1,24 +1,10 @@
 # vue-todo-app
 
-## Project setup
+An personal app to learn a lil more about the basics of Vue.js
+
+## Running this app
 ```
 npm install
-```
 
-### Compiles and hot-reloads for development
-```
 npm run serve
 ```
-
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/components/TodoApp.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="container">
+    <h1 class="title">Vue TODO App</h1>
+
+    <div class="inputs">
+      <input v-model="task" type="text" placeholder="Digite uma tarefa" class="task-input" v-on:keyup="handleKeyPress($event)" >
+      <button class="btn" @click="submitTask">Adicionar</button>
+    </div>
+  
+      <div class="tasks-table">
+        <div class="task-body" v-for="(task, index) in tasks" :key="index">
+          <p class="task-name" :class="{'finished': task.status === 'Feita'}" >{{firstLetterUpper(task.name)}}</p>
+          <p
+            @click="changeStatus(index)"
+            class="task-status"
+            :class="{
+              'danger': task.status === 'A fazer',
+              'good': task.status ===  'Feita'
+              }"
+          >
+            {{firstLetterUpper(task.status)}}
+          </p>
+          <p class="task-edit" @click="editTask(index)" ><span class="fa fa-pen"></span></p>
+          <p class="task-delete" @click="deleteTask(index)" ><span class="fa fa-trash"></span></p>
+        </div>
+      </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoApp",
+  
+  data(){
+    return{
+      task: "",
+      editedTask: null,
+      tasksStatus: ["A fazer", "Feita"],
+      tasks:[]
+    }
+  },
+
+  methods: {
+    handleKeyPress(event){
+      if(event.key === 'Enter') return this.submitTask()
+    },
+
+    submitTask(){
+      if(this.task.length === 0) return
+
+      if(this.editedTask === null){
+        this.tasks.push({
+          name: this.task,
+          status: "A fazer",
+        })
+      }else{
+        this.tasks[this.editedTask].name = this.task
+        this.editedTask = null
+      }
+      
+      this.task = ""
+    },
+
+    deleteTask(index){
+      this.tasks.splice(index, 1)
+    },
+
+    editTask(index){
+      this.task = this.tasks[index].name
+      this.editedTask = index
+    },
+
+    changeStatus(index){
+      let newIndex = this.tasksStatus.indexOf(this.tasks[index].status)
+      if(++newIndex > 1) newIndex = 0
+      this.tasks[index].status = this.tasksStatus[newIndex]
+    },
+
+    firstLetterUpper(status){
+      return status.charAt(0).toUpperCase() + status.slice(1)
+    }
+
+  }
+
+}
+</script>
+
+<style lang="scss">
+  @import "../assets/styles.scss";
+</style>
